7c9147a4c4b2ecc8075f4ad4d4664473f0a2de71
James Hawk <jph94880@gmail.com>
2023-05-29 16:45:49 -0500

    Adds my file

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
+import {city, state} from "../data/data.js"
+
 
 function About() {
   return (
@@ -7,10 +9,12 @@ function About() {
     About
     <h2>About Me</h2>
     <p>I'm a Web Developer from {city}, {state}</p>
-    <p>I like to take long walks on the beach and play with puppies</p>
+    {/* <p>I like to take long walks on the beach and play with puppies</p> */}
     <img src={image} alt="I made this" />
   </div>
   );
 }
 
 export default About;
+
+
