9cc1b3bd809d65c71ee8cb00b78da7d2a09c769a
James Hawk <jph94880@gmail.com>
2023-05-30 12:01:56 -0500

    Adds my file

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,6 @@ import {city, state} from "../data/data.js"
 function About() {
   return (
   <div id="about">
-    About
     <h2>About Me</h2>
     <p>I'm a Web Developer from {city}, {state}</p>
     {/* <p>I like to take long walks on the beach and play with puppies</p> */}
